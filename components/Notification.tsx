3b4deba49a505657ea20965f5ab45a2cea43bf7a
Sougata <sougatamukherjee47@gmail.com>
2024-11-03 20:14:16 +0530

    first commit

---
+++ components/Notification.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Notification = () => {
+  return (
+    <div className="h-12 bg-red-500 text-white px-4 flex items-center text-center text-sm">
+      Notification
+    </div>
+  );
+};
+
+export default Notification;
